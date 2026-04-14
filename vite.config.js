a34df42d18cb66cc32d7509d5826f945cdeb2b22
malone8413-jpg <malone8413@gmail.com>
2026-04-14 21:29:10 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': new URL('./src', import.meta.url).pathname,
-    },
-  },
+  base: '/OCM/',
 })
